b27f338e7bad11881be84d3fa0fa3134531b22fc
Loni Tande Miriam Ebenye <ebenyemiriam17@gmail.com>
2023-01-08 14:00:46 +0100

    Update guessingGame.py

--- guessingGame.py
+++ guessingGame.py
@@ -14,38 +14,38 @@
 
 import random
 import math
-def guessing_game_function():
-    name = input("Enter your name: ")
-    print("Welcome ", name)
-    lowerBound = 0
-    upperBound = 50
-    #generating random number,x,between set lowerbound and upperbound
-    x = random.randint(lowerBound, upperBound)
-    print("INSTRUCTION: You are to guess a number from 0 to 50")
-    print("NOTE: You've only ",
-           round(math.log(upperBound - lowerBound + 1, 2)),
-          " chances to guess the right number!\n")
-    #initializing number of guesses
-    count = 0
-    while count < math.log(upperBound - lowerBound + 1, 2):
-        count += 1
-        guess = int(input("Guess a number:- "))
-        if x == guess:
-            print("Congratulations you guessed after ",
-                  count, " tries")
-            break
-        elif x > guess:
-            print("Hot")
-        elif x < guess:
-            print("cold")
-    #if user exhausts maximum number of guesses
-    if count >= math.log(upperBound - lowerBound + 1, 2):
-        print("\nThe number is %d" % x , "Try again")
+    def guessing_game_function():
+        name = input("Enter your name: ")
+        print("Welcome ", name)
+        lowerBound = 0
+        upperBound = 50
+        #generating random number,x,between set lowerbound and upperbound
+        x = random.randint(lowerBound, upperBound)
+        print("INSTRUCTION: You are to guess a number from 0 to 50")
+        print("NOTE: You've only ",
+               round(math.log(upperBound - lowerBound + 1, 2)),
+              " chances to guess the right number!\n")
+        #initializing number of guesses
+        count = 0
+        while count < math.log(upperBound - lowerBound + 1, 2):
+            count += 1
+            guess = int(input("Guess a number:- "))
+            if x == guess:
+                print("Congratulations you guessed after ",
+                      count, " tries")
+                break
+            elif x > guess:
+                print("Hot")
+            elif x < guess:
+                print("cold")
+        #if user exhausts maximum number of guesses
+        if count >= math.log(upperBound - lowerBound + 1, 2):
+            print("\nThe number is %d" % x , "Try again")
 
-guessing_game_function()
-
-answer = input("Do you wish to play again? YES or NO: ")
-if answer == "YES" or answer == "Yes" or answer == "yes":
     guessing_game_function()
-elif answer == "NO" or answer == "No" or answer == "no":
-    print("Close IDE")
+
+    answer = input("Do you wish to play again? YES or NO: ")
+    if answer == "YES" or answer == "Yes" or answer == "yes":
+        guessing_game_function()
+    elif answer == "NO" or answer == "No" or answer == "no":
+        print("Close IDE")
