65b0a3d21cc33446b686b053222e3d3fa850b988
Loni Tande <ebenyemiriam17@gmail.com>
2023-06-09 12:06:13 +0200

    Add files via upload

---
+++ Basic Calculator.py
@@ -0,0 +1,24 @@
+name = input('What is your name? ')
+welcome_message = f'Hi {name}! What calculation would you like to perform today? \n' \
+                  f'A: Addition \n' \
+                  f'M: Multiplication \n' \
+                  f'D: Division \n' \
+                  f'S: Subtraction \n' \
+                  f'R: Remainder division'
+print(welcome_message)
+calculation= input('Enter letter: ').lower()
+number_1 = float(input('First number: '))
+number_2 = float(input('Second number: '))
+if calculation == 'a':
+    print(number_1 + number_2)
+elif calculation == 'm':
+    print(number_1 * number_2)
+elif calculation == 'd':
+    print(number_1 / number_2)
+elif calculation == 's':
+    print(number_1 - number_2)
+elif calculation == 'r':
+    print(number_1 % number_2)
+else:
+    print('Incorrect input')
+
