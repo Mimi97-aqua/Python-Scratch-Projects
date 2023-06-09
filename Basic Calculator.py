fa2b1969ac60feaa0ad3dae870670973fd2350ab
Loni Tande <ebenyemiriam17@gmail.com>
2023-06-09 11:14:20 +0100

    Update Basic Calculator.py
    Added the calculation for exponentiation

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -4,7 +4,8 @@ welcome_message = f'Hi {name}! What calculation would you like to perform today?
                   f'M: Multiplication \n' \
                   f'D: Division \n' \
                   f'S: Subtraction \n' \
-                  f'R: Remainder division'
+                  f'R: Remainder division \n'
+                  f'E: Exponentiation'
 print(welcome_message)
 calculation= input('Enter letter: ').lower()
 number_1 = float(input('First number: '))
@@ -19,6 +20,7 @@ elif calculation == 's':
     print(number_1 - number_2)
 elif calculation == 'r':
     print(number_1 % number_2)
+elif calculation == 'e':
+    print(number_1 ** number_2)
 else:
     print('Incorrect input')
-
